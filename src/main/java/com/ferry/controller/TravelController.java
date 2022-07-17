8986d6ae8b4980659efffcfce41cc3c27ba30051
王荣庆 <rongqing2@datatell.com.cn>
2022-07-17 12:40:49 +0800

    travelSites

---
+++ src/main/java/com/ferry/controller/TravelController.java
@@ -0,0 +1,23 @@
+package com.ferry.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * @description:
+ * @author:wangrongqing
+ * @createTime:2022 07 2022/7/17 12:39
+ * @version:1.0
+ */
+@RestController
+public class TravelController {
+
+    public Map<String, Object> findTravleSites(HttpServletRequest request, HttpServletResponse response) {
+
+        return null;
+    }
+
+}
