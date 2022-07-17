a1dee67eddd3c23ef03ccd7bbfafc2cb6dd2daf3
FerrymanJACK <wangrongqingJACK@163.com>
2022-07-17 13:04:24 +0800

    Update TravelController.java

--- src/main/java/com/ferry/controller/TravelController.java
+++ src/main/java/com/ferry/controller/TravelController.java
@@ -19,7 +19,7 @@ public class TravelController {
 
         System.out.println("client change");
         System.out.println("second client change");
-        String str = "20.205.243.166 github.com";
+        String str = "20.205.243.166 github.com-111";
         System.out.println("server change");
         System.out.println("second server change");
         return null;
