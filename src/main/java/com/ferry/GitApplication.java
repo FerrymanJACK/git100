9df7ae4ae751f99056bda1b24fd359b0590a9369
王荣庆 <rongqing2@datatell.com.cn>
2022-07-17 12:36:50 +0800

    init

---
+++ src/main/java/com/ferry/GitApplication.java
@@ -0,0 +1,20 @@
+package com.ferry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author:wangrongqing
+ * @createTime:2022 07 2022/7/17 12:23
+ * @version:1.0
+ */
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+
+}
